64ddef0d9d7b7cbe9f7298ad6cc941e58113f892
sahimakhtar <sahimakhtar1020@gmail.com>
2024-01-09 19:05:38 +0530

    removed files and folder and created orders table

--- src/index.js
+++ src/index.js
@@ -7,17 +7,10 @@ import "./styles/login.css";
 import "./styles/pricing.css";
 import "./styles/faqs.css";
 import "./styles/order_success.css";
-import { PersistGate } from "redux-persist/integration/react";
-import store, { persistor } from "./redux/store/store";
-import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
-      <Provider store={store}>
         <App />
-      </Provider>
     </BrowserRouter>
-  </PersistGate>
 );
